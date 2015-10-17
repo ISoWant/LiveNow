4d4f8a01b2116fdbeb7e9bb65b52f87495d9d2f8
ISoWant <a.s.evstifeev@gmail.ru>
2015-10-17 13:50:05 +0300

    Created MainActivity class and layout

---
+++ LiveNow/app/src/main/java/com/brothers/livenow/MainActivity.java
@@ -0,0 +1,15 @@
+package com.brothers.livenow;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Admin on 17.10.2015.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actifity_main);
+    }
+}
